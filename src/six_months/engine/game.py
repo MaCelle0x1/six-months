0ef5ab7efe536b2a731eaaddf0f3658abcaf8978
John <95731690+MaCelle0x1@users.noreply.github.com>
2026-08-18 14:42:31 -0500

    feat: allow game to receive player character

--- src/six_months/engine/game.py
+++ src/six_months/engine/game.py
@@ -10,5 +10,8 @@ class Game:
     def start(self) -> None:
         self.state.running = True
 
+    def set_player(self, character) -> None:
+        self.state.player = character
+
     def advance(self) -> None:
         self.state.advance_time()
